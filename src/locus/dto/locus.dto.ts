629e02d900751569120ede5d4590cc6c2f4f3d08
Mukhammad-amin-ai <aminahmadov123@gmail.com>
2026-03-25 14:34:12 +0500

    [u] updated docs

--- src/locus/dto/locus.dto.ts
+++ src/locus/dto/locus.dto.ts
@@ -11,6 +11,10 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
 
 export class GetLocusDto {
+  @ApiPropertyOptional({
+    description: 'Filter by locusIsd',
+    type: [Number],
+  })
   @IsOptional()
   @Transform(({ value }) => {
     if (value === undefined) return undefined;
@@ -26,10 +30,18 @@ export class GetLocusDto {
   @Type(() => Number)
   id?: number[];
 
+  @ApiPropertyOptional({
+    description: 'Filter by assemblyId',
+    type: Number,
+  })
   @IsOptional()
   @IsString()
   assemblyId?: string;
 
+  @ApiPropertyOptional({
+    description: 'Filter by regionId',
+    type: [Number],
+  })
   @IsOptional()
   @Transform(({ value }) => {
     if (value === undefined) return undefined;
@@ -44,6 +56,10 @@ export class GetLocusDto {
   @IsInt({ each: true })
   regionId?: number[];
 
+  @ApiPropertyOptional({
+    description: 'Filter by membershipStatus',
+    type: String,
+  })
   @IsOptional()
   @IsString()
   membershipStatus?: string;
@@ -56,22 +72,34 @@ export class GetLocusDto {
   @IsEnum(SideloadEnum)
   include?: SideloadEnum;
 
+  @ApiPropertyOptional({ description: 'Page number', default: 1 })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
   page?: number = 1;
 
+  @ApiPropertyOptional({ description: 'Rows per page', default: 1000 })
   @IsOptional()
   @IsInt()
   @Min(1)
   @Type(() => Number)
   limit?: number = 1000;
 
+  @ApiPropertyOptional({
+    description: 'Field to sort by',
+    enum: SortFieldEnum,
+    default: SortFieldEnum.ID,
+  })
   @IsOptional()
   @IsEnum(SortFieldEnum)
   sortBy?: SortFieldEnum = SortFieldEnum.ID;
 
+  @ApiPropertyOptional({
+    description: 'Sort Order',
+    enum: SortOrderEnum,
+    default: SortOrderEnum.ASC,
+  })
   @IsOptional()
   @IsEnum(SortOrderEnum)
   sortOrder?: SortOrderEnum = SortOrderEnum.ASC;
